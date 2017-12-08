a95351639833f5bc5105384d635162496f0bda30
Saurabh Kumar <saurabh.kumar@quovantis.com>
2017-12-08 18:11:24 +0530

    update routing

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::group(['middleware' => 'apiKeyAuth'], function () {
     Route::post('/user/create', 'UserController@create');
     Route::post('/user/login', 'UserController@login');
